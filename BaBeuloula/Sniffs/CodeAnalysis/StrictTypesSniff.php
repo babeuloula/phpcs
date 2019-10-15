36a6f287a51b800f0e304b85e0987514d7fab27d
BaBeuloula <babeuloula@gmail.com>
2019-10-15 20:41:53 +0200

    Add fixableError to add missing 'declare(strict_types=1);' (#7)

--- BaBeuloula/Sniffs/CodeAnalysis/StrictTypesSniff.php
+++ BaBeuloula/Sniffs/CodeAnalysis/StrictTypesSniff.php
@@ -2,6 +2,7 @@
 
 /**
  * @author BaBeuloula
+ * Fork of https://github.com/steevanb/php-code-sniffs
  */
 
 declare(strict_types=1);
@@ -14,15 +15,10 @@ use PHP_CodeSniffer\{
 };
 
 /**
- * Fork of https://github.com/steevanb/php-code-sniffs
- *
  * Force declare(strict_types=1)
  */
 class StrictTypesSniff implements Sniff
 {
-    /** @var bool[] */
-    protected $strictTypes = [];
-
     /** @return int[] */
     public function register(): array
     {
@@ -32,16 +28,61 @@ class StrictTypesSniff implements Sniff
     /** @param int $stackPtr */
     public function process(File $phpcsFile, $stackPtr): void
     {
-        if (T_DECLARE === $phpcsFile->getTokens()[$stackPtr]['code']) {
-            $this->strictTypes[$phpcsFile->getFilename()] = true;
-        } elseif (T_NAMESPACE === $phpcsFile->getTokens()[$stackPtr]['code']
-            && false === \array_key_exists($phpcsFile->getFilename(), $this->strictTypes)
-        ) {
-            $phpcsFile->addError(
-                'File should have "declare(strict_types=1);" before namespace',
+        $currentPtr = $phpcsFile->getTokens()[$stackPtr];
+
+        if ('T_NAMESPACE' === $currentPtr['type']) {
+            $declarePtr = $phpcsFile->findPrevious(
+                [T_DECLARE],
                 $stackPtr,
-                'StrictTypesRequired'
+                $phpcsFile->findPrevious([T_OPEN_TAG], $stackPtr)
             );
+
+            if (false === \is_int($declarePtr)) {
+                $isFixable = $phpcsFile->addFixableError(
+                    'File should have "declare(strict_types=1);" before namespace',
+                    $stackPtr,
+                    'StrictTypesRequired'
+                );
+
+                if (true === $isFixable) {
+                    $phpcsFile->fixer->beginChangeset();
+                    $phpcsFile->fixer->replaceToken(
+                        $stackPtr,
+                        'declare(strict_types=1);' . $phpcsFile->eolChar . $currentPtr['content']
+                    );
+                    $phpcsFile->fixer->endChangeset();
+                }
+            }
+        } else {
+            $strictTypePtr = $phpcsFile->findNext(
+                [T_STRING],
+                $stackPtr,
+                $phpcsFile->findNext([T_CLOSE_PARENTHESIS], $stackPtr)
+            );
+
+            $valuePtr = $phpcsFile->findNext(
+                [T_LNUMBER],
+                $stackPtr,
+                $phpcsFile->findNext([T_CLOSE_PARENTHESIS], $stackPtr)
+            );
+
+            if (true === \is_int($strictTypePtr)
+                && 'strict_types' === $phpcsFile->getTokens()[$strictTypePtr]['content']
+                && true === \is_int($valuePtr)
+                && '0' === $phpcsFile->getTokens()[$valuePtr]['content']
+            ) {
+                $isFixable = $phpcsFile->addFixableError(
+                    '"strict_types" must be equals to 1, not 0.',
+                    $valuePtr,
+                    'BadValueStrictTypes'
+                );
+
+                if (true === $isFixable) {
+                    $phpcsFile->fixer->beginChangeset();
+                    $phpcsFile->fixer->replaceToken($valuePtr, '1');
+                    $phpcsFile->fixer->endChangeset();
+                }
+            }
         }
     }
 }
